329347d0376bd647d69645e0dd0951da229adbf7
Luke Zhang <JustLuke336699@gmail.com>
2021-01-23 19:54:13 +0000

    Proj 1

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
-name =input("what is your name")
-print("hello,",name,"!")
+print("Welcome to My Greeting Program!")
+name =input("What is your name?")
+print("Hello,",name,"nice to meet you!")
+help = input ("Press Enter to Exit")
